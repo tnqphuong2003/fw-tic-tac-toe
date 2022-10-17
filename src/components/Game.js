6187bee216f4eff09bec18d85ca66e16d67bfc94
tnqphuong2003 <tnqphuong2003@gmail.com>
2022-10-17 16:51:29 +0700

    add history

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react";
 import Board from "./Board";
+import History from "../History";
 
 function Game() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
   const [xIsNext, setXIsNext] = useState(true);
   const [winner, setWinner] = useState(null);
+  const [stepNumber, setStepNumber] = useState(0);
 
   //Declaring a Winner
   useEffect(() => {
-    const newWinner = calculateWinner(squares);
+    const newWinner = calculateWinner(history[history.length - 1].squares);
     setWinner(newWinner);
-  }, [squares]);
+  }, [history]);
 
   // function to check if a player has won.
   // If a player has won, we can display text such as “Winner: X” or “Winner: O”.
@@ -41,18 +43,38 @@ function Game() {
 
   //Handle player
   const handleClick = (i) => {
-    const newSquares = squares.slice();
-    if (calculateWinner(newSquares) || newSquares[i]) return;
-    newSquares[i] = xIsNext ? "X" : "O";
-    setSquares(newSquares);
+    const currentHistory = history.slice(0, stepNumber + 1);
+    const current = currentHistory[currentHistory.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) return;
+    squares[i] = xIsNext ? "X" : "O";
+    setHistory(
+      currentHistory.concat([
+        {
+          squares: squares,
+        },
+      ])
+    );
     // chuyen doi player
+    setStepNumber(currentHistory.length);
     setXIsNext((prevStep) => !prevStep);
   };
 
   //Restart game
   const handleRestart = () => {
-    setSquares(Array(9).fill(null));
+    setStepNumber(0);
+    setHistory([
+      {
+        squares: Array(9).fill(null),
+      },
+    ]);
     setXIsNext(true);
+  };
+
+  //Undo game
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
   };
 
   return (
@@ -60,7 +82,13 @@ function Game() {
       <h2 className="result">Winner is: {winner ? winner : "N/N"}</h2>
       <div className="game">
         <span className="player">Next player is: {xIsNext ? "X" : "O"}</span>
-        <Board squares={squares} handleClick={handleClick} />
+        <div className="game-board">
+          <Board
+            squares={history[stepNumber].squares}
+            handleClick={handleClick}
+          />
+          <History history={history} jumpTo={jumpTo} />
+        </div>
       </div>
       <button onClick={handleRestart} className="restart-btn">
         Restart
